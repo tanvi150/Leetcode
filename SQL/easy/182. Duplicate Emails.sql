2b5c06b418caeb04db97058ec06073658ec6f65f
Tanvi <krishnamayitanvi@gmail.com>
2026-02-18 14:23:01 +0800

    Update 182. Duplicate Emails.sql

--- SQL/easy/182. Duplicate Emails.sql
+++ SQL/easy/182. Duplicate Emails.sql
@@ -2,8 +2,8 @@
 -- https://leetcode.com/problems/duplicate-emails/
 
 -- Problem: Report all the duplicate emails.
--- Approach: Use `GROUP BY` to group rows by email and `HAVING COUNT(*) > 1` to filter emails that appear more than once
--- Tip: If you also include `COUNT(*)` in the `SELECT` clause, you can see how many times each email appears.
+-- Approach: Use GROUP BY to group rows by email and HAVING COUNT(*) > 1 to filter emails that appear more than once
+-- Tip: If you also include COUNT(*) in the SELECT clause, you can see how many times each email appears.
 
 SELECT email AS Email FROM Person
 GROUP BY email HAVING COUNT(*) > 1;
